5bc9604cd5aeed3320adb25888193454e8457b65
2022302111171 <2781836100@qq.com>
2025-08-04 11:58:12 +0800

    返回修改

--- registry/src/main/java/com/psh/registry/RegistryApplication.java
+++ registry/src/main/java/com/psh/registry/RegistryApplication.java
@@ -5,10 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class RegistryApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(RegistryApplication.class);
